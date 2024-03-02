c18737144ec886f7e41a7544eea3cb3fc43f4ce2
JOKERadaw <65590227+JOKERadaw@users.noreply.github.com>
2024-03-02 20:47:55 +0100

    added points
    efafea

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script>
 	import Counter from './Counter.svelte';
 	import spacestation_fallback from '$lib/images/spacestation.png';
+	let totpoint=[0,0,0,-10];
+	$: sum = totpoint.reduce((acc, curr) => acc -  curr, 0);
+	function updatepoints(event, position){
+		totpoint[position]=event.detail.quantity
+		totpoint = [...totpoint]; 
+		
+		return totpoint
+	}
+
 </script>
 
 <svelte:head>
@@ -24,10 +33,11 @@
 	<h2>
 		welcome aboard <strong>CAPTAIN</strong>
 	</h2>
+	<h1>points: {sum}</h1>
 	<div class=counters>
-		<Counter material="STEEL"/>
-		<Counter material="MONEY"/>
-		<Counter material="FOOD"/>
+		<Counter material="STEEL" on:message={event=>totpoint=updatepoints(event,0)}/>
+		<Counter material="MONEY" on:message={event=>totpoint=updatepoints(event,1)}/>
+		<Counter material="FOOD" on:message={event=>totpoint=updatepoints(event,2)}/>
 	</div>
 
 </section>
--- my-app/src/routes/Counter.svelte
+++ my-app/src/routes/Counter.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { spring } from 'svelte/motion';
+	import { createEventDispatcher } from 'svelte';
 
+	const dispatch = createEventDispatcher();
 	let count = 0;
 	export let material="None";
 	const displayed_count = spring();
@@ -15,11 +17,19 @@
 		// handle negative numbers
 		return ((n % m) + m) % m;
 	}
+	function quantity() {
+		dispatch('message', {
+			quantity: count
+	});
+	}
 </script>
 
 <div class= fabric> 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
+	<button on:click={() => {
+		count -= 1; // Decrease the count by 1
+		quantity(); // Call the quantity() function
+	}} aria-label="Decrease the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5" />
 		</svg>
@@ -32,7 +42,10 @@
 		</div>
 	</div>
 
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+	<button on:click={() => {
+		count += 1; // increase the count by 1
+		quantity(); // Call the quantity() function
+	}} aria-label="Increase the counter by one">
 		<svg aria-hidden="true" viewBox="0 0 1 1">
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
